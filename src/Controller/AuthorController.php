b807b911a26c0452f59fee6aa29b7a546c45277f
selima <selima.bellil@esprit.tn>
2024-09-23 17:57:32 +0100

    first commit

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AuthorController extends AbstractController
+{   private $authors;
+    public function __construct(){
+            $this->authors=[
+                ['id'=>1, 'name'=>'Taha Hussain','nbrBooks'=>300,'picture'=>'images/th.jpeg','email'=>"taah@gmail.com"],
+                ['id'=>2, 'name'=>'Victor Hugo','nbrBooks'=>200,'picture'=>'images/vh.jpeg','email'=>"victor@gmail.com"],
+            ];
+    }
+   #[Route("/library",name:"app_library",methods:["GET"])]
+    public function index(){
+       return $this->render('author/index.html.twig');
+   }
+
+   #[Route("/author/{name}",
+       name:"app_author",
+       methods:["GET"],
+       defaults:["name"=>"taha hussain"])]
+   public function showAuthor($name){
+       return $this->render('author/show.html.twig',
+       array(
+           'name'=>$name
+       ));
+   }
+
+   //return list of authors
+   #[Route("/list",name:"app_list",methods:["GET"])]
+   public function authorList(){
+
+       return $this->render('author/list.html.twig',
+       [
+           'authors'=>$this->authors
+       ]);
+   }
+
+   #[Route ("/showDetails/{id}",name:"app_showDetail",methods:["GET"])]
+   
+    public function showDetailsAction(int $id): Response
+    {
+       
+        $author = null;
+        foreach ($this->authors as $a) {
+            if ($a['id'] == $id) {
+                $author = $a;
+                break;
+            }
+        }
+
+        
+        if (!$author) {
+            throw $this->createNotFoundException('Author not found.');
+        }
+
+        
+        return $this->render('author/showDetails.html.twig', [
+            'author' => $author
+        ]);
+    }
+}
+    
+    //var_dump($id);
+    //die();
+    //return $this->render("author/showDetails.html.twig",array());
+   //}
